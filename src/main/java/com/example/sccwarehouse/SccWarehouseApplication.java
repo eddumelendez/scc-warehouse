ce239397f2e53a4b3aeb59795f0bfcb8aa44ad5c
Eddú Meléndez <eddu.melendez@gmail.com>
2018-08-12 23:11:25 -0500

    Initial commit

---
+++ src/main/java/com/example/sccwarehouse/SccWarehouseApplication.java
@@ -0,0 +1,12 @@
+package com.example.sccwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SccWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SccWarehouseApplication.class, args);
+	}
+}
