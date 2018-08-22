903ee86d2d08c407818df1645ebe3983d4e1d894
Eddú Meléndez <eddu.melendez@gmail.com>
2018-08-22 00:53:09 -0500

    Add BaseContractTest and initial service implementation

---
+++ src/main/java/com/example/sccwarehouse/ProductService.java
@@ -0,0 +1,15 @@
+package com.example.sccwarehouse;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Eddú Meléndez
+ */
+@Service
+public class ProductService {
+
+	public ProductListResponse fetchProducts() {
+		return null;
+	}
+
+}
---
+++ src/test/java/com/example/sccwarehouse/BaseContractTest.java
@@ -0,0 +1,32 @@
+package com.example.sccwarehouse;
+
+import java.util.Arrays;
+import java.util.List;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+/**
+ * @author Eddú Meléndez
+ */
+public class BaseContractTest {
+
+	@Before
+	public void setup() {
+		List<Product> products = Arrays.asList(
+				new Product(1L, "Hulk Pop", "FU32690", "Coming in September 2018", "$ 10.99"),
+				new Product(2L, "Groot with Stormbreaker Pop", "FU35773", "Coming in December 2018", "$ 10.99"),
+				new Product(3L, "Bucky with weapon", "FU35775", "Coming in December 2018", "$ 10.99"));
+		ProductListResponse listOfProducts = new ProductListResponse();
+		listOfProducts.setProducts(products);
+
+		ProductService productService = mock(ProductService.class);
+		given(productService.fetchProducts()).willReturn(listOfProducts);
+
+		RestAssuredMockMvc.standaloneSetup(new ProductController(productService));
+	}
+
+}
